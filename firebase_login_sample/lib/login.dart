ec34cdbb2f15892edfb586a1317d890a641a2b34
Shejza <shejzaberisha@gmail.com>
2019-11-20 22:17:35 +0100

    Working on login with google

--- firebase_login_sample/lib/login.dart
+++ firebase_login_sample/lib/login.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_login_sample/home_page.dart';
+import 'package:firebase_login_sample/sign_in.dart';
 import 'package:flutter/material.dart';
 import 'auth.dart';
+
 
 class LoginPage extends StatefulWidget {
 
@@ -91,56 +94,101 @@ void moveToLogin() {
           key: formKey,
           child: new Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: buildInputs() + buildSubmitButton(),
+            children: buildInputs() + buildSubmitButton() +
+             signInButton()
           ),
           ),
       ),
     );
   }
 
-  List<Widget> buildInputs() {
-    return [ 
-    new TextFormField(
-                decoration: new InputDecoration(labelText: 'Email'),
-                validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
-                onSaved: (value) => _email = value,
-              ),
-              new TextFormField(
-                  decoration: new InputDecoration(
-                    labelText: 'Password'),
-                    obscureText:true,
-                     validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
-                     onSaved: (value) => _password = value,
-                  
-                ),
-];
-}
-List<Widget> buildSubmitButton() {
-  if(_formType == FormType.login){  
-  return [
-               new RaisedButton(
-                  child: new Text('Login', style: new TextStyle(fontSize: 20.0),),
-                  onPressed: validateAndSubmit,
-                
-              ),
-              new FlatButton(
-                child: new Text("Create an account", style: new TextStyle(fontSize: 20.0)) ,
-                onPressed: moveToRegister,
-              ),
-  ];
-} else { 
-  return [
-  new RaisedButton(
-                  child: new Text('Create an account', style: new TextStyle(fontSize: 20.0),),
-                  onPressed: validateAndSubmit,
-                
-              ),
-              new FlatButton(
-                child: new Text("Have an account? Login", style: new TextStyle(fontSize: 20.0)) ,
-                onPressed: moveToLogin,
-              ),
-  ];
+  
 
-}
-}
+              List<Widget> buildInputs() {
+                return [ 
+                new TextFormField(
+                            decoration: new InputDecoration(labelText: 'Email'),
+                            validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+                            onSaved: (value) => _email = value,
+                          ),
+                          new TextFormField(
+                              decoration: new InputDecoration(
+                                labelText: 'Password'),
+                                obscureText:true,
+                                validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+                                onSaved: (value) => _password = value,
+                              
+                            ),
+            ];
+            }
+
+              List<Widget> buildSubmitButton() {
+                if(_formType == FormType.login){  
+                return [
+                            new RaisedButton(
+                                child: new Text('Login', style: new TextStyle(fontSize: 20.0),),
+                                onPressed: validateAndSubmit,
+                              
+                            ),
+                            new FlatButton(
+                              child: new Text("Create an account", style: new TextStyle(fontSize: 20.0)) ,
+                              onPressed: moveToRegister,
+                            ),
+                ];
+              } else { 
+                return [
+                new RaisedButton(
+                                child: new Text('Create an account', style: new TextStyle(fontSize: 20.0),),
+                                onPressed: validateAndSubmit,
+                              
+                            ),
+                            new FlatButton(
+                              child: new Text("Have an account? Login", style: new TextStyle(fontSize: 20.0)) ,
+                              onPressed: moveToLogin,
+                            ),
+                ];
+
+              }
+              }
+              
+              List<Widget> signInButton() {
+                  return [OutlineButton(
+                    splashColor: Colors.grey,
+                    onPressed: () {
+                        signInWithGoogle().whenComplete(() {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return HomePage();
+                              },
+                            ),
+                          );
+                        });
+                      },
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                    highlightElevation: 0,
+                    borderSide: BorderSide(color: Colors.grey),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Text(
+                              'Sign in with Google',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                  ];
+                }
 }
